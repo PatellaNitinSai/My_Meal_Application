d5ae7de5f251c67ce8207f9b627563490b770ee2
PatellaNitinSai <nitinsaipatella2024@gmail.com>
2025-01-19 11:36:41 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect('mongodb+srv://nitinsaipatella1234:Nitin12sai@cluster0.iohfco5.mongodb.net/Manu');
+        const conn = await mongoose.connect('mongodb+srv://nitinsaipatella1234:Nitin12sai@cluster0.qvumu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Manu');
         console.log(`Mongoose Connected ${conn.connection.host}`)
     } catch (error) {
         console.log(error);
